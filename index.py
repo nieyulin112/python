40185918c89f14700b1e73861068a343099367a4
nieyulin112 <1139723906@qq.com>
2016-08-22 17:57:11 +0800

    first commit

--- index.py
+++ index.py
@@ -1,4 +1,23 @@
+import os
 movices = ['author',123,'activeor', {
 }]
 for item in movices:
 	print(item)
+
+names = ['jhon',['china','japanse'],'xyz']
+print(names, True, 4)
+
+data = open('test.txt')
+for item in data:
+    print(item)
+data.close()
+
+
+if os.path.exists('test.txt'):
+    list = open('test.txt')
+
+    for l in list:
+        print('l', l)
+    list.close()
+else:
+    print('the datafile is missing')
